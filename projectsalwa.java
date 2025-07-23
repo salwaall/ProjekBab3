bb417c342b088d66978475ee448d350d12c03470
salwaall <putrisalwaalthoffunnisa@gmail.com>
2025-07-23 14:45:26 +0700

    Add files via upload

---
+++ projectsalwa.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyekbaru;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class projectsalwa {
+    public static void main(String[] args) throws IOException {
+//      System.out.println("hello world");
+//      System.out.println("Halo"+" "+"Dunia");
+//      System.out.println(2+" "+5);
+//      System.out.println("Angka "+" "+5);
+//      System.out.println("2 + 5"+" = "+2+5);  
+//      System.out.println("2 + 5"+" = "+(2+5));
+//      System.out.println("2 + 2" + (2+2));
+
+                
+
+
+//        String namaDepan = "";
+//        String namaBelakang = "";
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("Nama depan: ");
+//        namaDepan = input.next();
+//
+//        System.out.print("Nama belakang: ");
+//        namaBelakang = input.next();
+//
+//        System.out.println("Nama saya adalah: " + namaDepan + " " + namaBelakang);
+
+
+
+//        String namaDepan = "";
+//        String namaBelakang = "";
+//
+//        
+//        InputStreamReader ireader = new InputStreamReader(System.in);
+//        
+//        BufferedReader breader = new BufferedReader(ireader);
+//
+//        System.out.println("Nama Depan : ");
+//        
+//        namaDepan = breader.readLine();
+//
+//        System.out.println("Nama Belakang : ");
+//        
+//        namaBelakang = breader.readLine();
+//
+//        System.out.println("Nama Saya adalah :");
+//        System.out.println(namaDepan + " " + namaBelakang);
+
+    
+//  { String nama =
+//JOptionPane.showInputDialog("Masukan Nama :");
+//System.out.println("Nama : "+nama);}
+
+
+//String a =
+//JOptionPane.showInputDialog("Masukan nilaiuntuk variabel a : ");
+//
+//String b =
+//JOptionPane.showInputDialog("Masukan nilaiuntuk variabel b : ");
+//
+//System.out.println("Hasil :" +
+//(Integer.parseInt(a) *
+//Integer.parseInt(b)));
+
+//String nama = "Salwa";
+//String jurusan = "PPLG";
+//JOptionPane.showMessageDialog(null, "Halo " + nama
+//+ ",jurusan Kamu " + jurusan);
+
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Informasi.","Informasi",
+//JOptionPane.INFORMATION_MESSAGE);
+
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Error !","Error",
+//JOptionPane.ERROR_MESSAGE);
+
+
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Peringatan.","Peringatan",
+//JOptionPane.WARNING_MESSAGE);
+
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Pertanyaan.", "Pertanyaan",
+//JOptionPane.QUESTION_MESSAGE);
+
+//JOptionPane.showMessageDialog(
+//null, "Sebuah Pertanyaan.", "Pertanyaan",
+//JOptionPane.QUESTION_MESSAGE);
+
+
+JOptionPane.showConfirmDialog(null, "Pilih Yes atau No?", "Pertanyaan",JOptionPane.YES_NO_OPTION);
+        int pertanyaan = 0;
+if(JOptionPane.YES_OPTION == pertanyaan){
+JOptionPane.showMessageDialog(null, "Kamu memilih Yes.");
+}else if(JOptionPane.NO_OPTION == pertanyaan){
+JOptionPane.showMessageDialog(null, "Kamu memilih No.");}}
+     
+
+        }
+
